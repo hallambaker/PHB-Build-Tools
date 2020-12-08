edc998497ea9b0fea1dbc1b30bf11223e6474444
PHB <phill@hallambaker.com>
2020-12-08 12:40:01 -0500

    Make Unknown tag always have value -1. Fix misc project misconfigs

--- DocTools/bootmaker/AssemblyVersion.cs
+++ DocTools/bootmaker/AssemblyVersion.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyVersionAttribute("3.0.0.395")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("3.0.0.395")]
+[assembly: System.Reflection.AssemblyVersionAttribute("3.0.0.396")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("3.0.0.396")]
 
--- RegistryConfig/registryconfig/AssemblyVersion.cs
+++ RegistryConfig/registryconfig/AssemblyVersion.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyVersionAttribute("3.0.0.371")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("3.0.0.371")]
+[assembly: System.Reflection.AssemblyVersionAttribute("3.0.0.372")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("3.0.0.372")]
 
