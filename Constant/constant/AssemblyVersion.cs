5de19a267ce0a711286e0bda8b97a4dd96e8b7ab
PHB <phill@hallambaker.com>
2020-12-08 02:13:26 -0500

    Published build

--- Constant/constant/AssemblyVersion.cs
+++ Constant/constant/AssemblyVersion.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyVersionAttribute("3.0.0.454")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("3.0.0.454")]
+[assembly: System.Reflection.AssemblyVersionAttribute("3.0.0.458")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("3.0.0.458")]
 
