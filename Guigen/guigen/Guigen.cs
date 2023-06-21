fa7f5e9d5ea820f1a91e0402e800c684717d151c
phill@hallambaker.com <phill@hallambaker.com>
2023-06-21 01:40:43 -0400

    Add binding methods to protogen output

--- Guigen/guigen/Guigen.cs
+++ Guigen/guigen/Guigen.cs
@@ -1,5 +1,5 @@
 ﻿
-//  This file was automatically generated at 19-Jun-23 3:16:08 PM
+//  This file was automatically generated at 21-Jun-23 1:38:29 AM
 //   
 //  Changes to this file may be overwritten without warning
 //  
