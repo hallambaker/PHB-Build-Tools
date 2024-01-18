28279f2db3d03e1d939b9777da35f27df40fa4cb
Phill Hallam-Baker <hallam@gmail.com>
2024-01-18 12:00:01 -0500

    Tweak gui params

--- Guigen/guigen/AssemblyVersion.cs
+++ Guigen/guigen/AssemblyVersion.cs
@@ -1,4 +1,4 @@
 // These values are automatically generated from version.version
-[assembly: System.Reflection.AssemblyVersionAttribute("3.0.0.1495")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("3.0.0.1495")]
+[assembly: System.Reflection.AssemblyVersionAttribute("3.0.0.1496")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("3.0.0.1496")]
 
